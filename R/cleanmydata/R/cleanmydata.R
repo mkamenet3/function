9a5602f446161bbaca8c2fc88471827a4df70f6d
Maria Kamenetsky <maria.kamenetsky@gmail.com>
2015-04-10 13:13:31 -0500

    Added another function for converting multiple vars at once to facors

--- R/cleanmydata/R/cleanmydata.R
+++ R/cleanmydata/R/cleanmydata.R
@@ -2,3 +2,9 @@ cleanmydata <-function(mydf) {
 	mydf[mydf==""] <-NA
 	mydf[complete.cases(mydf),]
 	}
+
+factorme <- function(mydf, vars) {
+	factors <- which(names(mydf)%in%vars)
+	mydf[,factors]<- lapply(mydf[,factors], as.factor)
+	return(mydf)
+	}
