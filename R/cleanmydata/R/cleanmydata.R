5a1392ea46af2bd9a141d9228b9a548d9ad318f0
Maria Kamenetsky <maria.kamenetsky@gmail.com>
2015-04-14 11:55:21 -0500

    fixed bug in numerics part

--- R/cleanmydata/R/cleanmydata.R
+++ R/cleanmydata/R/cleanmydata.R
@@ -4,7 +4,7 @@ cleanmydata <-function(mydf, factorVars, numericVars) {
     factors <- which(names(mydf)%in%factorVars)
     mydf[,factors]<- lapply(mydf[,factors], as.factor)
     numerics <- which(names(mydf)%in%numericVars)
-    mydf[,numerics]<-lapply(mydf[,numerics], as.numeric)
+    mydf[,numerics]<-lapply(mydf[,numerics, drop=FALSE], as.numeric)
     return(mydf)    
 }
 
