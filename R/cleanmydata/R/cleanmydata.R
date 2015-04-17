fe541b6962dcdfb6b0e343db326c489e86fc9e7c
Maria Kamenetsky <maria.kamenetsky@gmail.com>
2015-04-17 10:18:46 -0500

    modified function so now dropping NA is a TRUE or FALSE option

--- R/cleanmydata/R/cleanmydata.R
+++ R/cleanmydata/R/cleanmydata.R
@@ -1,6 +1,9 @@
-cleanmydata <-function(mydf, factorVars, numericVars) {
+cleanmydata <-function(mydf, factorVars, numericVars, drop_na=TRUE) {
     mydf[mydf==""] <-NA
-    mydf <- mydf[complete.cases(mydf),]
+    if(drop_na) {
+        mydf <- mydf[complete.cases(mydf),]    
+    }
+    mydf
     factors <- which(names(mydf)%in%factorVars)
     mydf[,factors]<- lapply(mydf[,factors], as.factor)
     numerics <- which(names(mydf)%in%numericVars)
