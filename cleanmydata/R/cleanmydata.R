d81812f4836acd504bc337f7307a9accad841b23
Maria Kamenetsky <maria.kamenetsky@gmail.com>
2015-05-01 14:02:40 -0500

    this is the version that was roxygen2-ed

--- cleanmydata/R/cleanmydata.R
+++ cleanmydata/R/cleanmydata.R
@@ -1,3 +1,12 @@
+#' Basic cleaning of data
+#' 
+#' @param mydf use cbind.data.frame() to create sub-dataset with your variables of interest. Specify that 'stringAsFactors=FALSE"
+#' @param factorVars variables you want to convert to factors
+#' @param numericVars variables you want to convert to numeric
+#' @param drop_na true or false variables if you want to drop missing variables or not
+#' @return new dataframe with cleaned variables 
+
+
 cleanmydata <-function(mydf, factorVars, numericVars, drop_na=TRUE) {
     mydf[mydf==""] <-NA
     if(drop_na) {
